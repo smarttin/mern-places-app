f520dd843e6dc091e784e1bcbfdeb844a014547b
smarttin <smarttin77@gmail.com>
2020-04-14 17:44:51 +0100

    more styling with component

---
+++ src/shared/components/UIElements/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Card.css';
+
+const Card = props => {
+  console.log(props.className)
+  return (
+    <div className={`card ${props.className}`} style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
