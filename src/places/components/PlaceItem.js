19f6d77a1e9c8a0e9fdcd9cc7c0ca10a91cf67fd
smarttin <smarttin77@gmail.com>
2020-04-16 17:33:06 +0100

    client delete place

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -9,10 +9,24 @@ import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = ({ id, title, image, description, address, coordinates }) => {
   const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
 
   const closeMapHandler = () => setShowMap(false);
+
+  const deleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  }
+
+  const cancelDeleteWarningHandler = () => {
+    setShowConfirmModal(false);
+  }
+
+  const confirmDeleteHandler = () => {
+    console.log("Deleting...")
+    setShowConfirmModal(false);
+  }
 
   return (
     <Fragment>
@@ -28,6 +42,22 @@ const PlaceItem = ({ id, title, image, description, address, coordinates }) => {
           <Map center={coordinates} zoom={16} />
         </div>
       </Modal>
+
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteWarningHandler}
+        header="Are you sure"
+        footerClass="place-item__modal-actions"
+        footer={
+          <>
+            <Button inverse onClick={cancelDeleteWarningHandler}>CANCEL</Button>
+            <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+          </>
+        }
+      >
+        <p>Do you wish to delete place? please note this action is irreversible</p>
+      </Modal>
+
       <li className="place-item">
         <Card style={{ padding: 0 }}>
           <div className="place-item__image">
@@ -41,7 +71,7 @@ const PlaceItem = ({ id, title, image, description, address, coordinates }) => {
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/places/${id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={deleteWarningHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
